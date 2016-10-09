567c0eb979baae73712cdc8cd23f620f09e743ef
Geovani de Souza <geovanisouza92@gmail.com>
2016-10-09 14:57:17 -0300

    feat(cycle-scripts-es-browserify): Working build script

--- cycle-scripts-es-browserify/scripts/build.js
+++ cycle-scripts-es-browserify/scripts/build.js
@@ -1,13 +1,22 @@
-var fs = require('fs')
+var fs = require('fs-extra')
 var {join} = require('path')
+var mkdirp = require('mkdirp')
 var browserify = require('browserify')
 var babelify = require('babelify')
 
-browserify(join('src', 'index.js'))
+var buildPath = join(process.cwd(), 'build')
+var publicPath = join(process.cwd(), 'public')
+var srcPath = join(process.cwd(), 'src')
+
+mkdirp.sync(buildPath)
+
+fs.copySync(publicPath, buildPath)
+
+browserify(join(srcPath, 'index.js'))
   .transform(
     babelify.configure({
       presets: ['es2015']
     })
   )
   .bundle()
-  .pipe(fs.createWriteStream(join('build', 'bundle.js')))
+  .pipe(fs.createWriteStream(join(buildPath, 'bundle.js')))
