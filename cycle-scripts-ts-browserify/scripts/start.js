a80978a34c82d088be6fa642e5829d4ba479fd2f
Geovani de Souza <geovani@serrabits.com.br>
2016-10-11 14:34:23 -0300

    WIP(cycle-scripts-ts-browserify): start script

--- cycle-scripts-ts-browserify/scripts/start.js
+++ cycle-scripts-ts-browserify/scripts/start.js
@@ -1,1 +1,31 @@
 'use strict'
+
+var path = require('path')
+var budo = require('budo')
+var tsify = require('tsify')
+// var envify = require('envify/custom')
+// var hotModuleReload = require('browserify-hmr')
+
+// require('dotenv').config({silent: true})
+
+budo(path.join('src', 'index.ts'), {
+  serve: 'bundle.js',
+  dir: 'public',
+  live: true,
+  port: 8000,
+  stream: process.stdout,
+  browserify: {
+    plugin: [
+      // hotModuleReload,
+      tsify
+    ],
+    debug: true,
+    insertGlobals: true
+    // transform: [
+    //   envify(Object.assign({}, process.env, {
+    //     _: 'purge',
+    //     NODE_ENV: 'development'
+    //   }))
+    // ]
+  }
+})
