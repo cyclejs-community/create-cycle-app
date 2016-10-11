abfb4829e43023f80dfb79307c7160345344d2af
Geovani de Souza <geovani@serrabits.com.br>
2016-10-11 11:35:14 -0300

    feat(cycle-scripts-ts-browserify): init script

---
+++ cycle-scripts-ts-browserify/template/src/app.test.ts
@@ -0,0 +1,9 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+
+import assert from 'assert'
+
+describe('App', () => {
+  it('should test something', () => {
+    // TODO: Add your tests here
+  })
+})
