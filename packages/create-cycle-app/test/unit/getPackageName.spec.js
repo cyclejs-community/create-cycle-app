55d4d5e5a228902309954524ba04673cc7715edf
Nick Balestra <nbalestra@opentable.com>
2017-01-22 16:21:07 +0000

    unit tests for cli

--- packages/create-cycle-app/test/unit/getPackageName.spec.js
+++ packages/create-cycle-app/test/unit/getPackageName.spec.js
@@ -1,8 +1,15 @@
 var getPackageName = require('../../src/getPackageName')
 
 describe('getPackageName module', function () {
-  test('should return the unaltered package name if alrady given it', function () {
+  test('should return the unaltered package name if alrady given', function () {
     expect(getPackageName('cycle-scripts')).toBe('cycle-scripts')
+    expect(getPackageName('are-we-there-yet-1.1.2.tgz')).toBe('are-we-there-yet')
+    expect(getPackageName('@module/sub')).toBe('module')
   })
-  // TODO: complete tests
+  test('should return the correct package name if inside a .tgz', function () {
+    expect(getPackageName('are-we-there-yet-1.1.2.tgz')).toBe('are-we-there-yet')
+  })
+  test('should return the correct package name if uses a @/ naming', function () {
+    expect(getPackageName('@module/sub')).toBe('module')
+  })
 })
--- packages/create-cycle-app/test/unit/installScripts.spec.js
+++ packages/create-cycle-app/test/unit/installScripts.spec.js
@@ -1,11 +1,84 @@
+var path = require('path')
+
+jest.mock('cross-spawn')
+var spawn = require('cross-spawn')
+spawn.mockImplementation(() => ({ on: jest.fn() }))
+jest.mock('../../src/getPackageName')
+var getPackageName = require('../../src/getPackageName')
+jest.mock('console')
+var consoleMock = require('console')
+
 var installScripts = require('../../src/installScripts')
 
-installScripts = jest.fn(function () { return 1 })
-
-describe('installScripts module', function () {
-  test('should...', function () {
-    installScripts('test', 'appName', 'cycle-scripts')
-    expect(installScripts()).toBe(1)
+describe('installScripts module', () => {
+  test('should be a function with arity 4', () => {
+    expect(typeof installScripts).toBe('function')
+    expect(installScripts.length).toBe(4)
   })
-  // TODO: complete tests
+
+  describe('when invoked with a flavor published on npm', () => {
+    getPackageName.mockImplementation(name => name)
+    installScripts('./', 'appName', 'cycle-scripts')
+
+    test('should call getPackageName with the name of the npm module', () => {
+      expect(getPackageName).toBeCalledWith('cycle-scripts')
+    })
+
+    test('should call spawn with the correct arguments', () => {
+      expect(spawn).toBeCalledWith(
+        'npm',
+        [
+          'install',
+          '--save-dev',
+          '--save-exact',
+          'cycle-scripts'
+        ],
+        {'stdio': 'inherit'}
+      )
+    })
+
+    test('should console log the correct information', function () {
+      expect(consoleMock.log.mock.calls[0][0]).toContain(
+        'Installing packages. This might take a couple minutes.',
+      )
+      expect(consoleMock.log.mock.calls[1][0]).toContain(
+        'Installing cycle-scripts from npm ...',
+      )
+      expect(consoleMock.log.mock.calls[2][0]).toBe()
+    })
+  })
+
+  describe('when invoked with a local flavor', () => {
+    getPackageName.mockImplementation(name => path.basename(name))
+    installScripts('./', 'appName', './cycle-scripts')
+
+    test('should console log the correct information', () => {
+      expect(consoleMock.log.mock.calls[4][0]).toContain(
+        'Installing cycle-scripts from local',
+      )
+    })
+
+    test('should call spawn with the correct arguments', () => {
+      expect(spawn.mock.calls[1][1][3]).toContain('/create-cycle-app/packages/create-cycle-app/cycle-scripts')
+    })
+  })
+
+  describe('when invoked with a verbose flag', () => {
+    getPackageName.mockImplementation(name => name)
+    installScripts('./', 'appName', 'cycle-scripts', true)
+
+    test('should call spawn with the correct arguments', () => {
+      expect(spawn.mock.calls[2]).toMatchObject([
+        'npm',
+        [
+          'install',
+          '--verbose',
+          '--save-dev',
+          '--save-exact',
+          'cycle-scripts'
+        ],
+        {'stdio': 'inherit'}
+      ])
+    })
+  })
 })
