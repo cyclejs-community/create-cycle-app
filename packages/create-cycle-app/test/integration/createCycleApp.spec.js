f9cc35d021c63ae56807922b30277e7e01645d43
Nick Balestra <nbalestra@opentable.com>
2017-01-28 21:19:32 +0000

    test cleanup

--- packages/create-cycle-app/test/integration/createCycleApp.spec.js
+++ packages/create-cycle-app/test/integration/createCycleApp.spec.js
@@ -4,25 +4,34 @@ const appName = path.resolve(__dirname, 'MYCYCLEAPP')
 const spawn = require('cross-spawn')
 
 describe('create-cycle-app', () => {
-  afterEach(() => {
-    fs.removeSync(appName)
-  })
-  beforeEach((done) => {
-    spawn.sync('node', [
-      path.resolve(__dirname, '../../index.js'),
-      appName
-    ])
-    done()
-  })
-  test('', () => {
-    const dir = fs.readdirSync(appName)
-    expect(dir).toMatchObject([
-      '.gitignore',
-      'node_modules',
-      'package.json',
-      'public',
-      'src'
-    ])
-    expect(fs.readFileSync(path.join(appName, 'package.json'), 'UTF8')).toMatchSnapshot()
+  describe('when invoked with the a directory', () => {
+    let dir
+
+    beforeEach((done) => {
+      spawn.sync('node', [
+        path.resolve(__dirname, '../../index.js'),
+        appName
+      ])
+      dir = fs.readdirSync(appName)
+      done()
+    })
+
+    afterEach(() => {
+      fs.removeSync(appName)
+    })
+
+    test('it generate the correct folder structure and install dependencies', () => {
+      expect(dir).toMatchObject([
+        '.gitignore',
+        'node_modules',
+        'package.json',
+        'public',
+        'src'
+      ])
+    })
+
+    test('it generate a correct package.json file', () => {
+      expect(fs.readFileSync(path.join(appName, 'package.json'), 'UTF8')).toMatchSnapshot()
+    })
   })
 })
